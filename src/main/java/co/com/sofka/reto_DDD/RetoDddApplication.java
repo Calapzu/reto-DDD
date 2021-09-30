8f155da6136352ef29ec70a370b122118de200c0
Calapzu <yamsoncalapzu@gmail.com>
2021-09-29 21:28:48 -0500

    Se crea el proyecto para iniciar plantear el codigo de Dominio de la Veterinaria La Finca

---
+++ src/main/java/co/com/sofka/reto_DDD/RetoDddApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.reto_DDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoDddApplication.class, args);
+	}
+
+}
